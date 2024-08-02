f3b3013729871a02af6d493d9bfabb9640bbef70
Bijoy Kar <bijoykar54321@gmail.com>
2024-08-02 21:48:26 +0530

    feat: Search bar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+// Libraries Import
+import Link from "next/link";
+
 // Components Import
 import Banners from "@/components/Banners";
 
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="w-[300px] sm:w-[600px] md:w-[610px] lg:w-[930px] h-full">
+      <div className="w-[300px] sm:w-[600px] md:w-[650px] lg:w-[970px] h-full">
+        <Link href="/">
+          <h1 className="text-3xl font-bold mt-4">BannerGen</h1>
+        </Link>
         <Banners />
       </div>
     </div>
--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -2,6 +2,7 @@
 
 // Libraries import
 import { useState, useRef, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 
 // Data import
 import data from "@/data/banner-data.json";
@@ -12,22 +13,57 @@ import BannerImageComp from "@/components/BannerImageComp";
 
 export default function Banners() {
   const [isEdit, setIsEdit] = useState(false);
+  const [bannresData, setBannersData] = useState(data);
   const [editBannerId, setEditBannerId] = useState<number | string>(0);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
-  const dialogRef = useRef<HTMLDialogElement>(null);
+  const editDialogRef = useRef<HTMLDialogElement>(null);
+  const searchDialogRef = useRef<HTMLDialogElement>(null);
 
   // Function to open the edit dialog
   const openEdit = (id: number | string) => {
     setIsEdit(true);
     setEditBannerId(id);
-    dialogRef.current?.showModal();
+    editDialogRef.current?.showModal();
   };
 
   // Function to close the edit dialog
   const closeEdit = () => {
     setIsEdit(false);
     setEditBannerId(0);
-    dialogRef.current?.close();
+    editDialogRef.current?.close();
+  };
+
+  // Function to open the search dialog
+  const openSearch = () => {
+    setIsSearchOpen(true);
+    searchDialogRef.current?.showModal();
+  };
+
+  // Function to close the search dialog
+  const closeSearch = () => {
+    setIsSearchOpen(false);
+    searchDialogRef.current?.close();
+  };
+
+  // Function to search the banners
+  const searchBanners = () => {
+    if (searchValue) {
+      const searchResults = data.filter(
+        (item) =>
+          item.title?.text.toLowerCase().includes(searchValue.toLowerCase()) ||
+          item.description?.text
+            .toLowerCase()
+            .includes(searchValue.toLowerCase())
+      );
+      setBannersData(searchResults);
+      closeSearch();
+      return searchResults;
+    }
+    setBannersData(data);
+    closeSearch();
+    return data;
   };
 
   useEffect(() => {
@@ -37,8 +73,11 @@ export default function Banners() {
       }
     });
     window.addEventListener("click", (e) => {
-      if (e.target === dialogRef.current) {
+      if (e.target === editDialogRef.current) {
         closeEdit();
+      }
+      if (e.target === searchDialogRef.current) {
+        closeSearch();
       }
     });
     return () => {
@@ -48,7 +87,7 @@ export default function Banners() {
         }
       });
       window.removeEventListener("click", (e) => {
-        if (e.target === dialogRef.current) {
+        if (e.target === editDialogRef.current) {
           closeEdit();
         }
       });
@@ -56,14 +95,22 @@ export default function Banners() {
   }, [isEdit]);
 
   return (
-    <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-3 my-12">
-        {data.map((item) => (
+    <div className="flex">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-3 my-12">
+        {bannresData.map((item) => (
           <BannerImageComp key={item.id} openEdit={openEdit} data={item} />
         ))}
       </div>
+      <div className="hidden sm:flex justify-center w-14 z-40 mt-12">
+        <button
+          onClick={openSearch}
+          className="sticky top-10 flex items-center justify-center w-10 h-10 bg-slate-600/50 rounded hover:bg-slate-600/70"
+        >
+          <FaSearch className="text-white text-xl" />
+        </button>
+      </div>
       <dialog
-        ref={dialogRef}
+        ref={editDialogRef}
         className="z-50 rounded backdrop:backdrop-blur-lg backdrop:w-screen backdrop:h-screen backdrop:bg-slate-600/50 bg-white"
       >
         <EditBannerTemplateBs
@@ -71,6 +118,30 @@ export default function Banners() {
           closeEdit={closeEdit}
           data={data}
         />
+      </dialog>
+      <dialog
+        ref={searchDialogRef}
+        className="z-50 w-[90vw] md:w-[600px] lg:w-[920px] bg-transparent relative -top-[250px] m-auto"
+        id="search-modal"
+      >
+        <form
+          action={searchBanners}
+          className="flex items-center justify-center w-full h-full"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search..."
+            className="w-full p-2 border-2 rounded-lg outline-none border-slate-600"
+          />
+          <button
+            type="submit"
+            className="ml-1 w-10 h-10 flex justify-center items-center bg-slate-600/50 rounded hover:bg-slate-600/70"
+          >
+            <FaSearch className="text-white text-xl" />
+          </button>
+        </form>
       </dialog>
     </div>
   );
